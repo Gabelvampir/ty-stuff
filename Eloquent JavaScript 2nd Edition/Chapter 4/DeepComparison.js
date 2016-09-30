d97afa6b84281b9b76e03ef8bc1b201ecf5799b2
Gabelvampir <scotty_d@gmx.net>
2016-10-01 00:24:43 +0200

    éarly stages of solution of Deep Comparison exercise

---
+++ Eloquent JavaScript 2nd Edition/Chapter 4/DeepComparison.js
@@ -0,0 +1,14 @@
+function deepEqual(value1, value2) {
+	if (typeof value1 != object && typeof value2 != object) {
+		if (value1 === value2) return true;
+		else return false;
+	}
+}
+
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// ? true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// ? false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// ? true
