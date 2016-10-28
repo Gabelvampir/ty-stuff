b0d201c2c2458c5f70a53bada2584dc43242af6d
Gabelvampir <scotty_d@gmx.net>
2016-10-28 09:51:09 +0200

    took example solution of Deep Comparison exercise and adapted that to fill out the programm parts missing from my solution

--- Eloquent JavaScript 2nd Edition/Chapter 4/DeepComparison.js
+++ Eloquent JavaScript 2nd Edition/Chapter 4/DeepComparison.js
@@ -1,9 +1,26 @@
 function deepEqual(value1, value2) {
 	if (typeof value1 != "object" && typeof value2 != "object") {
 		if (value1 === value2) return true;
-		else return false;
 	}
+
+	if (value1 == null || typeof value1 != "object" ||
+		value2 == null || typeof value2 != "object")
+	return false;
+  
+	var propsIn1 = 0, propsIn2 = 0;
+
+	for (var prop in value1)
+		propsIn1 += 1;
+
+	for (var prop in value2) {
+		propsIn2 += 1;
+		if (!(prop in value1) || !deepEqual(value1[prop], value2[prop]))
+			return false;
+	}
+
+		return propsIn1 == propsIn2;
 }
+
 
 var obj = {here: {is: "an"}, object: 2};
 console.log(deepEqual(obj, obj));
